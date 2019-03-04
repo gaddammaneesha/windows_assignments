0c52ac84834a0c60be26ff30a6202bd6c7ddfc00
Maneesha <gaddammaneesha2@gmail.com>
2019-03-04 09:57:36 -0800

    event

---
+++ Assignment1/create_event/event.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<tchar.h>
+#include<shellapi.h>
+//creating event orogram
+int main()
+{
+	SECURITY_ATTRIBUTES se;
+	se.bInheritHandle = TRUE;
+	se.lpSecurityDescriptor = NULL;
+	se.nLength = sizeof(se);
+	//creating event
+	HANDLE hEve = CreateEvent(
+		&se, FALSE, FALSE, L"ANVESH");
+	//checking whether event is created or not
+	if (hEve == INVALID_HANDLE_VALUE)
+		printf("Event Creation failed\n");
+	else
+		printf("DONE - [%d]\n", GetLastError());
+	//closing the handle
+	CloseHandle(hEve);
+
+	getchar();
+	return 0;
+}
