aa5e16237c35eb29fcf0c140024a3077b00f7837
Maneesha <gaddammaneesha2@gmail.com>
2019-03-05 14:54:37 -0800

    critical section

--- Assignment1/critical section/Source.cpp
+++ Assignment1/critical section/Source.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<windows.h>
 CRITICAL_SECTION obj;
-long int glo_var = 0;
+long int glo_var;
 DWORD WINAPI thread_func1(LPVOID lparam)
 {
 	EnterCriticalSection(&obj);
@@ -24,20 +24,24 @@ int main()
 	DWORD dwthread_id1, dwthread_id2;
 	HANDLE arr[2];
 	InitializeCriticalSection(&obj);
-	hthread1 = CreateThread(NULL, 0, thread_func1, NULL, 0, &dwthread_id1);
-	if (hthread1 == NULL)
-		printf("thread1 not created\n");
-	else
-		arr[0] = hthread1;
-	hthread2 = CreateThread(NULL, 0, thread_func2, NULL, 0, &dwthread_id2);
-	if (hthread2 == NULL)
-		printf("thread2 not created\n");
-	else
-		arr[1] = hthread2;
-	WaitForMultipleObjects(2, arr, TRUE, 1000);
-	printf("value of shared variable is %d\n", glo_var);
+	while (1)
+	{
+		glo_var = 0;
+		hthread1 = CreateThread(NULL, 0, thread_func1, NULL, 0, &dwthread_id1);
+		if (hthread1 == NULL)
+			printf("thread1 not created\n");
+		else
+			arr[0] = hthread1;
+		hthread2 = CreateThread(NULL, 0, thread_func2, NULL, 0, &dwthread_id2);
+		if (hthread2 == NULL)
+			printf("thread2 not created\n");
+		else
+			arr[1] = hthread2;
+		WaitForMultipleObjects(2, arr, TRUE, 1000);
+		printf("value of shared variable is %d\n", glo_var);
+		CloseHandle(hthread1);
+		CloseHandle(hthread2);
+	}
 	DeleteCriticalSection(&obj);
-	CloseHandle(hthread1);
-	CloseHandle(hthread2);
 	getchar();
 }
